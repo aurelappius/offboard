24c335e099229950d3329f3deae17ec88a10bc15
aurel <appiusa@student.ethz.ch>
2022-03-08 08:57:21 +0100

    python plotter

--- log/Plot.py
+++ log/Plot.py
@@ -18,30 +18,31 @@ fig_xyz = plt.figure()  # xyz plot
 z_plt = fig_xyz.add_subplot(111)
 
 # Data initialization / Read Data
-pathToData = sys.argv[1]
-df = pd.read_csv(pathToData)
+
+#df = pd.read_csv(sys.argv[2])
+
 
 header_list = [
     "t",
     "z_ref",
     "z",
-]  # old format
+]
 
-t = pd.read_csv(
-    pathToData, usecols=["t"], skiprows=Start, nrows=N, names=header_list
+df = pd.read_csv(sys.argv[1])
+Curve1 = pd.read_csv(
+    sys.argv[1], names=header_list
 ).to_numpy()
+z_plt.scatter(Curve1[:, 0], Curve1[:, 2], marker="o", s=1, c=ref_color)
+z_plt.scatter(Curve1[:, 0], Curve1[:, 1], marker="o", s=1, c=curve_color)
 
-z_ref = pd.read_csv(
-    pathToData, usecols=["z_ref"], skiprows=Start, nrows=N, names=header_list
+df = pd.read_csv(sys.argv[2])
+Curve2 = pd.read_csv(
+    sys.argv[2], names=header_list
 ).to_numpy()
-
-z = pd.read_csv(
-    pathToData, usecols=["z"], skiprows=Start, nrows=N, names=header_list
-).to_numpy()
+#z_plt.scatter(Curve2[:, 0], Curve2[:, 2], marker="o", s=1, c=ref_color)
+z_plt.scatter(Curve2[:, 0], Curve2[:, 1]-0.9, marker="o", s=1, c=curve_color)
 
 
-z_plt.scatter(t, z_ref, marker="o", s=1, c=ref_color)
-z_plt.scatter(t, z, marker="o", s=1, c=curve_color)
 z_plt.set_xlabel("time [s]")
 z_plt.set_ylabel("Z [mm]")
 
