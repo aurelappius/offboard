32190296f6aa638ca0d56342cfdc3b8b4af38f88
aurel <appiusa@student.ethz.ch>
2022-03-16 15:28:25 +0100

    added Cheeseman compensator

--- app/src/quad_control.cpp
+++ app/src/quad_control.cpp
@@ -10,32 +10,32 @@
 #include <thread>
 #include <time.h>
 
-// FastDDS
-#include "domain_participant.h"
-#include "sub_callback.h"
-#include "subscriber.h"
 // MAVSDK
 #include <mavsdk/mavsdk.h>
 #include <mavsdk/plugins/action/action.h>
 #include <mavsdk/plugins/offboard/offboard.h>
 #include <mavsdk/plugins/telemetry/telemetry.h>
 
-// parameters
-#include "parameter_list.h"
 // yaml
 #include <yaml-cpp/yaml.h>
+
+// helpers
+#include "mavsdk_helper.h"
+#include "yaml_helper.h"
 
 using namespace mavsdk;
 using std::chrono::milliseconds;
 using std::chrono::seconds;
 using std::this_thread::sleep_for;
 
-// constants
-const float g = 9.81;                // m_s2
-const float quadcopter_mass = 1.5;   // kg
-const float max_thrust = 4 * 8.9764; // N
+/* CONSTANTS */
+const float g = 9.81;                 // gravitational acceleration [m_s2]
+const float quadcopter_mass = 1.5;    // Quadcopter mass [kg]
+const float max_thrust = 4 * 8.9764;  // maximal thrust [N]
+const float quad_rotor_radius = 0.12; // Quadcopter rotor radius [m]
 
-// thrust-throttle relation
+/* FUNCTION DECLARATIONS */
+// thrust-throttle relation (linear)
 float thrust_to_throttle(float thrust) {
   if (thrust > max_thrust) {
     return 1;
@@ -46,159 +46,38 @@ float thrust_to_throttle(float thrust) {
   return (0.02394 * thrust + 0.1644);
 }
 
-// Checks whether given yaml file exists
-inline void yaml_file_check(std::string yaml_file) {
-
-  try {
-    if (std::filesystem::exists(yaml_file) == false)
-      throw(yaml_file);
-  } catch (std::string yaml_file) {
-    std::cerr << "YAML file error: " << yaml_file << " does not exist" << '\n';
-    std::exit(EXIT_FAILURE);
+// reference generation
+void trajectory_generator(float t, Eigen::Vector3f &pos,
+                          Eigen::Vector3f &pos_ref, float &yaw_ref) {
+  if (t < 15) { // takeoff
+    pos_ref(0) = pos(0);
+    pos_ref(1) = pos(1);
+    pos_ref(2) = 2;
+    yaw_ref = 0;
+  } else { // fly circles
+    pos_ref(0) = std::sin(t / 5);
+    pos_ref(1) = std::cos(t / 5);
+    pos_ref(2) = 2;
+    yaw_ref = 0;
   }
 }
 
-inline void set_parameters(const std::string setpoint_path) {
-
-  // check if yaml file exists
-  yaml_file_check(setpoint_path);
-  // Load yaml file containing gains
-  YAML::Node commands_yaml = YAML::LoadFile(setpoint_path);
-
-  // Set parameters
-  params::SIM = commands_yaml["SIM"].as<bool>();
-  params::v = commands_yaml["v"].as<float>();
-  params::h_0 = commands_yaml["h_0"].as<float>();
-  params::freq = commands_yaml["freq"].as<float>();
-  params::amp = commands_yaml["amp"].as<float>();
-  params::P_vel_XY = commands_yaml["P_vel_XY"].as<float>();
-  params::I_vel_XY = commands_yaml["I_vel_XY"].as<float>();
-  params::D_vel_XY = commands_yaml["D_vel_XY"].as<float>();
-  params::P_pos_XY = commands_yaml["P_pos_XY"].as<float>();
-  params::P_vel_Z = commands_yaml["P_vel_Z"].as<float>();
-  params::I_vel_Z = commands_yaml["I_vel_Z"].as<float>();
-  params::D_vel_Z = commands_yaml["D_vel_Z"].as<float>();
-  params::P_pos_Z = commands_yaml["P_pos_Z"].as<float>();
-  params::T_s = commands_yaml["T_s"].as<int>();
-  params::k_h = commands_yaml["k_h"].as<double>();
-  params::hover = commands_yaml["hover"].as<float>();
-  params::T = commands_yaml["T"].as<int>();
-}
-
-void usage(const std::string &bin_name) {
-  std::cerr
-      << "Usage : " << bin_name << " <connection_url>\n"
-      << "Connection URL format should be :\n"
-      << " For TCP : tcp://[server_host][:server_port]\n"
-      << " For UDP : udp://[bind_host][:bind_port]\n"
-      << " For Serial : serial:///path/to/serial/dev[:baudrate]\n"
-      << "For example, to connect to the simulator use URL: udp://:14540\n";
-}
-
-std::shared_ptr<System> get_system(Mavsdk &mavsdk) {
-  std::cout << "Waiting to discover system...\n";
-  auto prom = std::promise<std::shared_ptr<System>>{};
-  auto fut = prom.get_future();
-
-  // We wait for new systems to be discovered, once we find one that has an
-  // autopilot, we decide to use it.
-  mavsdk.subscribe_on_new_system([&mavsdk, &prom]() {
-    auto system = mavsdk.systems().back();
-
-    if (system->has_autopilot()) {
-      std::cout << "Discovered autopilot\n";
-
-      // Unsubscribe again as we only want to find one system.
-      mavsdk.subscribe_on_new_system(nullptr);
-      prom.set_value(system);
-    }
-  });
-
-  // We usually receive heartbeats at 1Hz, therefore we should find a
-  // system after around 3 seconds max, surely.
-  if (fut.wait_for(seconds(3)) == std::future_status::timeout) {
-    std::cerr << "No autopilot found.\n";
-    return {};
-  }
-
-  // Get discovered system now.
-  return fut.get();
+// Cheeseman compensator
+float CheesemanCompensator(float throttle_ref, float z) {
+  return throttle_ref / (1.0 - std::pow((quad_rotor_radius / (4 * z)), 2));
 }
 
 int main(int argc, char **argv) {
-
-  // load yaml parameters
+  /* LOAD YAML PARAMETERS */
   set_parameters("app/parameters/params.yaml");
 
-  // // FASTDDS
-  // // Create participant. Arguments-> Domain id, QOS name
-  // DefaultParticipant dp(0, "mocap_subscriber");
-
-  // // Create publisher with msg type
-  // DDSSubscriber mocap_sub(idl_msg::MocapPubSubType(), &sub::mocap_msg,
-  //                         "mocap_srl_quad", dp.participant());
-  // if (!params::SIM) {
-  //   // activate mocap
-  //   for (int i = 0; i < 10; i++) {
-  //     mocap_sub.listener->wait_for_data();
-  //     std::cout << "x= " << sub::mocap_msg.pose.position.x << std::endl;
-  //   }
-  //   std::cout << "initialized mocap" << std::endl;
-  // }
-
-  // LOGGING
-
-  // std::ofstream myLog;
-
-  // std::string Name_0 = "flight";
-  // std::string suffix = "_nr_0";
-  // std::string Name_h0 = "_h0_";
-  // std::string val_h0 = std::to_string(params::h_0);
-  // val_h0.erase(val_h0.find_last_not_of('0') + 2, std::string::npos);
-  // std::string Name_v = "_v_";
-  // std::string val_v = std::to_string(params::v);
-  // val_v.erase(val_v.find_last_not_of('0') + 2, std::string::npos);
-  // std::string Name_freq = "_freq_";
-  // std::string val_freq = std::to_string(params::freq);
-  // val_freq.erase(val_freq.find_last_not_of('0') + 2, std::string::npos);
-  // std::string Name_amp = "_amp_";
-  // std::string val_amp = std::to_string(params::amp);
-  // val_amp.erase(val_amp.find_last_not_of('0') + 2, std::string::npos);
-  // std::string Name_sim;
-  // if (params::SIM == true) {
-  //   Name_sim = "_SIM";
-  // } else {
-  //   Name_sim = "_REAL";
-  // }
-  // std::string Name = Name_0 + Name_h0 + val_h0 + Name_v + val_v + Name_freq +
-  //                    val_freq + Name_amp + val_amp + Name_sim + suffix;
-
-  // // check if file already exists
-  // for (int i = 0;; i++) {
-  //   Name.pop_back();
-  //   Name = Name + std::to_string(i);
-  //   std::filesystem::path f{"log/" + Name + ".csv"};
-  //   if (!(std::filesystem::exists(f))) {
-  //     break;
-  //   }
-  // }
-
-  // myLog.open("log/" + Name + ".csv");
-
-  // std::cout << "Started logging to log/" << Name << ".csv\n";
-
-  // contdown [for testing]
-  // for (int i = 3; i > 0; i--) {
-  //   std::cout << "T minus " << i << std::endl;
-  //   sleep_for(seconds(1));
-  // }
-  // std::cout << "LIFTOFF!" << std::endl;
+  /* INITIALIZE LOGGING */
   std::ofstream myLog;
   std::string Name = "temp";
   myLog.open("log/" + Name + ".csv");
   std::cout << "Started logging to log/" << Name << ".csv\n";
 
-  /// MAVSDK
+  /* INITIALIZE MAVSDK */
   if (argc != 2) {
     usage(argv[0]);
     return 1;
@@ -217,13 +96,13 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // Instantiate plugins.
-  auto action = Action{system};
-  auto offboard = Offboard{system};
-  auto telemetry = Telemetry{system};
+  auto action = Action{system};       // for arming / disarming etc
+  auto offboard = Offboard{system};   // for offboard control
+  auto telemetry = Telemetry{system}; // for telemetry services
 
   std::cout << "System is ready\n";
 
+  /* ARM QUADCOPTER */
   const auto arm_result = action.arm();
   std::cerr << "Arming Result: " << arm_result << '\n';
 
@@ -233,7 +112,6 @@ int main(int argc, char **argv) {
   // sleep_for(seconds(10));
 
   /* SEND OFFBOARD ONCE BEFORE STARTING (otherwise it will be rejected) */
-
   // velocity command
   // Offboard::VelocityNedYaw vel_cmd{};
   // offboard.set_velocity_ned(vel_cmd);
@@ -264,10 +142,11 @@ int main(int argc, char **argv) {
   float yaw_ref = 0;
 
   // current states
-  Eigen::Vector3f pos(0, 0, 0);             // position
-  Eigen::Vector3f vel(0, 0, 0);             // velocity
-  Eigen::Quaternion<float> att(0, 0, 0, 0); // attitude quaternion
-  Eigen::Matrix3f body_frame;               // rotation matrix
+  Eigen::Vector3f pos(0, 0, 0);                  // position
+  Eigen::Vector3f vel(0, 0, 0);                  // velocity
+  Eigen::Quaternion<float> att_quat(0, 0, 0, 0); // attitude quaternion
+  Eigen::Vector3f att_euler(0, 0, 0);            // attitude euler angles
+  Eigen::Matrix3f body_frame;                    // rotation matrix
 
   // controller errors
   Eigen::Vector3f pos_p_error(0, 0, 0);
@@ -279,12 +158,9 @@ int main(int argc, char **argv) {
   float t = 0;
   const float T_s_sec = float(params::T_s) / 1000.0;
 
-  for (int i = 0;; i++) {
+  for (int i = 0;; i++) { // control loop at 50Hz
     // indices -> real-time
     t = float(i * params::T_s) / 1000.0;
-
-    // trajectory generation
-    trajectory_generator(pos_ref, yaw_ref);
 
     /* CURRENT STATE */
     // current position
@@ -295,13 +171,20 @@ int main(int argc, char **argv) {
     vel(0) = telemetry.position_velocity_ned().velocity.north_m_s;
     vel(1) = telemetry.position_velocity_ned().velocity.east_m_s;
     vel(2) = -telemetry.position_velocity_ned().velocity.down_m_s;
+    // current orientation (euler angles)
+    att_euler(0) = telemetry.attitude_euler().roll_deg;
+    att_euler(1) = telemetry.attitude_euler().pitch_deg;
+    att_euler(2) = telemetry.attitude_euler().yaw_deg;
     // current orientation (quaternion)
-    att.w() = telemetry.attitude_quaternion().w;
-    att.x() = telemetry.attitude_quaternion().x;
-    att.y() = telemetry.attitude_quaternion().y;
-    att.z() = telemetry.attitude_quaternion().z;
+    att_quat.w() = telemetry.attitude_quaternion().w;
+    att_quat.x() = telemetry.attitude_quaternion().x;
+    att_quat.y() = telemetry.attitude_quaternion().y;
+    att_quat.z() = telemetry.attitude_quaternion().z;
     //  body frame (rotation matrix)
-    body_frame = att.toRotationMatrix();
+    body_frame = att_quat.toRotationMatrix();
+
+    /* TRAJECTORY GENERATION */
+    trajectory_generator(t, pos, pos_ref, yaw_ref);
 
     /* POSITION CONTROLLER */
     // proportional position error
@@ -376,23 +259,29 @@ int main(int argc, char **argv) {
 
     // attitude commands (negative sign to account for xyz -> NED coordinate
     // change)
+
     att_cmd.roll_deg = -euler_ref(0) * (180.0 / M_PI);
     att_cmd.pitch_deg = -euler_ref(1) * (180.0 / M_PI);
     att_cmd.yaw_deg = -euler_ref(2) * (180.0 / M_PI);
-    att_cmd.thrust_value = throttle_ref;
+
+    // att_cmd.thrust_value = throttle_ref;
+    att_cmd.thrust_value = CheesemanCompensator(throttle_ref, pos(2));
     offboard.set_attitude(att_cmd);
 
     /* LOGGING*/
-    // t, p_ref, p
-    if (t > 0) { // wait for transients to fade away
+    // t, p_ref, p, rpy
+    if (t > params::T_log) { // possibility to wait for transients to fade away
       myLog << t << "," << pos_ref(0) << "," << pos_ref(1) << "," << pos_ref(2)
-            << "," << pos(0) << "," << pos(1) << "," << pos(2) << "\n";
+            << "," << pos(0) << "," << pos(1) << "," << pos(2) << ","
+            << att_euler(0) << "," << att_euler(1) << "," << att_euler(2)
+            << "\n";
     }
 
     /* SLEEP */
     sleep_for(milliseconds(params::T_s)); // 50Hz
   }
 
+  /* DISARM QUADCOPTER */
   sleep_for(seconds(5));
   const auto disarm_result = action.disarm();
   std::cout << "Disarming Result: " << disarm_result << '\n';
