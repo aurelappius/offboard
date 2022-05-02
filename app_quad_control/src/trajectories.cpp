2a603c3ea58dfab3883c6488bf2ab2d9402db415
aurel <appiusa@student.ethz.ch>
2022-05-02 23:15:57 +0200

    added circle trajectory

--- app_quad_control/src/trajectories.cpp
+++ app_quad_control/src/trajectories.cpp
@@ -4,11 +4,14 @@
 void stepResponse(float t, Eigen::Vector3f &pos,
                   Eigen::Vector3f &pos_ref, float &yaw_ref)
 {
+    const float h_0 = 1.2; // height before step [m]
+    const float h_1 = 0.2; // height after step [m]
+
     if (t > 0 && t <= 20)
     { // takeoff
         pos_ref(0) = 0;
         pos_ref(1) = 0;
-        pos_ref(2) = 1.20;
+        pos_ref(2) = h_0;
         yaw_ref = 0.0;
     }
     // step response
@@ -16,7 +19,7 @@ void stepResponse(float t, Eigen::Vector3f &pos,
     {
         pos_ref(0) = 0;
         pos_ref(1) = 0;
-        pos_ref(2) = 0.20;
+        pos_ref(2) = h_1;
         yaw_ref = 0.0;
     }
     if (t > 55)
@@ -33,7 +36,48 @@ void stepResponse(float t, Eigen::Vector3f &pos,
 void circleStepResponse(float t, Eigen::Vector3f &pos,
                         Eigen::Vector3f &pos_ref, float &yaw_ref)
 {
-    // TODO
+    // constants
+    const float h_0 = 1.2;                                // height before step [m]
+    const float h_1 = 0.2;                                // height after step [m]
+    const float speed = 1.0;                              // horizontal speed [m_s]
+    const float circle_Radius = 1.0;                      // radius of flown circle [m]
+    const float angular_velocity = speed / circle_Radius; // angular velocity [1_s]
+    const float circle_center_x = 1.0;                    // center of circle [m]
+    const float circle_center_y = 1.0;                    // center of circle [m]
+
+    // takeoff and go to start
+    if (t > 0 && t <= 20)
+    {
+        pos_ref(0) = std::cos(0);
+        pos_ref(1) = std::sin(0);
+        pos_ref(2) = h_0;
+        yaw_ref = 0.0;
+    }
+    // start flying circles
+    if (t > 20 && t <= 40)
+    {
+        pos_ref(0) = std::cos(angular_velocity * t);
+        pos_ref(1) = std::sin(angular_velocity * t);
+        pos_ref(2) = h_0;
+        yaw_ref = angular_velocity * t;
+    }
+    // step down while flying circles
+    if (t > 40 && t <= 60)
+    {
+        pos_ref(0) = std::cos(angular_velocity * t);
+        pos_ref(1) = std::sin(angular_velocity * t);
+        pos_ref(2) = h_1;
+        yaw_ref = angular_velocity * t;
+    }
+
+    if (t > 60)
+    { // land
+        std::cout << "landing now" << std::endl;
+        pos_ref(0) = pos(0);
+        pos_ref(1) = pos(1);
+        pos_ref(2) = 0.0;
+        yaw_ref = 0.0;
+    }
 }
 
 // oscillations
