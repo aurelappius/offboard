49de05cb21c4ce1c77911930c2372812a1c4c48f
aurel <appiusa@student.ethz.ch>
2022-05-03 09:12:31 +0200

    added new trajectories

--- app_quad_control/src/trajectories.cpp
+++ app_quad_control/src/trajectories.cpp
@@ -39,35 +39,35 @@ void circleStepResponse(float t, Eigen::Vector3f &pos,
     // constants
     const float h_0 = 1.2;                                // height before step [m]
     const float h_1 = 0.2;                                // height after step [m]
-    const float speed = 1.0;                              // horizontal speed [m_s]
-    const float circle_Radius = 1.0;                      // radius of flown circle [m]
-    const float angular_velocity = speed / circle_Radius; // angular velocity [1_s]
+    const float speed = 0.5;                              // horizontal speed [m_s]
+    const float circle_radius = 1.0;                      // radius of flown circle [m]
+    const float angular_velocity = speed / circle_radius; // angular velocity [1_s]
     const float circle_center_x = 1.0;                    // center of circle [m]
     const float circle_center_y = 1.0;                    // center of circle [m]
 
     // takeoff and go to start
     if (t > 0 && t <= 20)
     {
-        pos_ref(0) = std::cos(0);
-        pos_ref(1) = std::sin(0);
+        pos_ref(0) = circle_center_x + circle_radius * std::cos(0);
+        pos_ref(1) = circle_center_y + circle_radius * std::sin(0);
         pos_ref(2) = h_0;
         yaw_ref = 0.0;
     }
     // start flying circles
     if (t > 20 && t <= 40)
     {
-        pos_ref(0) = std::cos(angular_velocity * t);
-        pos_ref(1) = std::sin(angular_velocity * t);
+        pos_ref(0) = circle_center_x + circle_radius * std::cos(angular_velocity * (t - 20));
+        pos_ref(1) = circle_center_y + circle_radius * std::sin(angular_velocity * (t - 20));
         pos_ref(2) = h_0;
-        yaw_ref = angular_velocity * t;
+        yaw_ref = angular_velocity * (t - 20);
     }
     // step down while flying circles
     if (t > 40 && t <= 60)
     {
-        pos_ref(0) = std::cos(angular_velocity * t);
-        pos_ref(1) = std::sin(angular_velocity * t);
+        pos_ref(0) = circle_center_x + circle_radius * std::cos(angular_velocity * (t - 20));
+        pos_ref(1) = circle_center_y + circle_radius * std::sin(angular_velocity * (t - 20));
         pos_ref(2) = h_1;
-        yaw_ref = angular_velocity * t;
+        yaw_ref = angular_velocity * (t - 20);
     }
 
     if (t > 60)
@@ -92,7 +92,57 @@ void oscillations(float t, Eigen::Vector3f &pos,
 void staticDataCollection(float t, Eigen::Vector3f &pos,
                           Eigen::Vector3f &pos_ref, float &yaw_ref)
 {
-    // TODO
+    // constants
+    const float h_0 = 1.2;                    // height before step [m]
+    const float h_1 = 0.1;                    // height after step [m]
+    const float h_step = 0.05;                // step size [m]
+    const int n_steps = (h_0 - h_1) / h_step; // number of steps [ ]
+    const float x = 0.5;                      // x-coordinate [m]
+    const float y = 0.5;                      // y-coordinate [m]
+
+    // find OGE thrust
+    if (t > 0 && t <= 30)
+    {
+        pos_ref(0) = x;
+        pos_ref(1) = y;
+        pos_ref(2) = h_0;
+        yaw_ref = 0.0;
+    }
+    // go down in steps of h_step
+    if (t > 30 && t <= 60)
+    {
+        int i = float((t - 30.0) / 30.0 * n_steps);
+        pos_ref(0) = x;
+        pos_ref(1) = y;
+        pos_ref(2) = h_0 - i * h_step;
+        yaw_ref = 0.0;
+    }
+    // go up in steps of h_step
+    if (t > 60 && t <= 90)
+    {
+        int i = float((t - 60.0) / 30.0 * n_steps);
+        pos_ref(0) = x;
+        pos_ref(1) = y;
+        pos_ref(2) = h_1 + i * h_step;
+        yaw_ref = 0.0;
+    }
+    // hover a little
+    if (t > 90 && t <= 100)
+    {
+        pos_ref(0) = x;
+        pos_ref(1) = y;
+        pos_ref(2) = h_0;
+        yaw_ref = 0.0;
+    }
+    // land
+    if (t > 100)
+    {
+        std::cout << "landing now" << std::endl;
+        pos_ref(0) = x;
+        pos_ref(1) = y;
+        pos_ref(2) = 0.0;
+        yaw_ref = 0.0;
+    }
 }
 
 // vertical speed flying for more advanced GE Model
